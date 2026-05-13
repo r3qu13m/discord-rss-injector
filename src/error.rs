5fe0c10e57049264584884be2f593c2ea59ba197
r3qu13m <r3qu13m.minecraft@gmail.com>
2026-05-13 20:32:39 +0900

    Use thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,35 +1,11 @@
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum Error {
-    Reqwest(String),
-    FeedRs(String),
-    Io(String),
-    Json(String),
+    #[error("reqwest error")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("feed_rs error")]
+    FeedRs(#[from] feed_rs::parser::ParseFeedError),
+    #[error("std::io error")]
+    Io(#[from] std::io::Error),
+    #[error("serde_json error")]
+    Json(#[from] serde_json::Error),
 }
-
-macro_rules! impl_error {
-    ($name: ident, $target: ty) => {
-        impl From<$target> for Error {
-            fn from(value: $target) -> Self {
-                Self::$name(value.to_string())
-            }
-        }
-    };
-}
-
-impl_error!(Reqwest, reqwest::Error);
-impl_error!(FeedRs, feed_rs::parser::ParseFeedError);
-impl_error!(Io, std::io::Error);
-impl_error!(Json, serde_json::Error);
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        match self {
-            Self::Reqwest(e) => write!(f, "Reqwest error: {}", e),
-            Self::FeedRs(e) => write!(f, "feed_rs error: {}", e),
-            Self::Io(e) => write!(f, "std::io error: {}", e),
-            Self::Json(e) => write!(f, "serde_json error: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
